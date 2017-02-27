2fcadc706e9c9c84e41fa9324d56029fe22b0748
荒川由貴宏 <yukihiro.arakawa@gmail.com>
2017-02-28 06:01:33 +0900

    aaa

--- src/main/java/sample/JsonMakeRun.java
+++ src/main/java/sample/JsonMakeRun.java
@@ -1,15 +1,21 @@
 package sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import sample.service.JsonMakeService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class JsonMakeRun {
 
+	private static Logger log = LoggerFactory.getLogger(JsonMakeRun.class);
+
     public static void main(String[] args) throws JsonProcessingException{
+
+    	log.info("Starting my application...");
         JsonMakeRun jsonMakeRun = new JsonMakeRun();
         jsonMakeRun.execute();
     }
